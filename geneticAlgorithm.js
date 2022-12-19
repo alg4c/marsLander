796262945f8cc0a00a220fcc67e34a17bd7146de
Alexander <alexgriffith360@gmail.com>
2022-12-18 20:34:51 -0500

    Reset genetic algorithm

--- geneticAlgorithm.js
+++ geneticAlgorithm.js
@@ -5,33 +5,34 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateCmd() {
-  // generate random command string, clamp rotation: -15-15, thrust: 0-1
-  return `${random(-15, 16)} ${random(-1, 2)}`;
+function generateLetter() {
+  const code = random(97, 123); // ASCII char codes
+  return String.fromCharCode(code);
 }
 
 class Member {
-  constructor(nCmd) {
-    this.cmd = [];
+  constructor(target) {
+    this.target = target;
+    this.keys = [];
 
-    for (let i = 0; i < nCmd; i++) {
-      this.cmd[i] = generateCmd();
+    for (let i = 0; i < target.length; i += 1) {
+      this.keys[i] = generateLetter();
     }
   }
   fitness() {
-    //fitness ratio is a number between 0 and 1, 1 being most fit.
     let match = 0;
 
-    for (let i = 0; i < this.cmd.length; i += 1) {
-      if (this.cmd[i] === this.cmd[i]) {
+    for (let i = 0; i < this.keys.length; i += 1) {
+      if (this.keys[i] === this.target[i]) {
         match += 1;
       }
     }
-    return match / this.nCmd.length;
+
+    return match / this.target.length;
   }
   crossover(partner) {
-    const { length } = this.nCmd;
-    const child = new Member(this.nCmd);
+    const { length } = this.target;
+    const child = new Member(this.target);
     const midpoint = random(0, length);
 
     for (let i = 0; i < length; i += 1) {
@@ -56,13 +57,12 @@ class Member {
 }
 
 class Population {
-  constructor(size, nCmd, mutationRate = 0) {
+  constructor(size, target, mutationRate) {
     size = size || 1;
     this.members = [];
     this.mutationRate = mutationRate;
-
     for (let i = 0; i < size; i += 1) {
-      this.members.push(new Member(nCmd));
+      this.members.push(new Member(target));
     }
   }
   _selectMembersForMating() {
@@ -78,7 +78,6 @@ class Population {
         matingPool.push(m);
       }
     });
-
     return matingPool;
   }
   _reproduce(matingPool) {
@@ -86,13 +85,10 @@ class Population {
       // Pick 2 random members/parents from the mating pool
       const parentA = matingPool[random(0, matingPool.length)];
       const parentB = matingPool[random(0, matingPool.length)];
-
       // Perform crossover
       const child = parentA.crossover(parentB);
-
       // Perform mutation
       child.mutate(this.mutationRate);
-
       this.members[i] = child;
     }
   }
@@ -104,24 +100,22 @@ class Population {
   }
 }
 
-//function to run the GA
-function generate(populationSize, nCmd, mutationRate, generations) {
+function generate(populationSize, target, mutationRate, generations) {
   // Create a population and evolve for N generations
-  const population = new Population(populationSize, nCmd, mutationRate);
+  const population = new Population(populationSize, target, mutationRate);
   population.evolve(generations);
 
-  // Get the typed words from all members, and find if someone was able to type the nCmd
+  // Get the typed words from all members, and find if someone was able to type the target
   const membersKeys = population.members.map((m) => m.keys.join(""));
-  const perfectCandidatesNum = membersKeys.filter((w) => w === nCmd);
+  const perfectCandidatesNum = membersKeys.filter((w) => w === target);
 
   // Print the results
   console.log(membersKeys);
   console.log(
     `${
       perfectCandidatesNum ? perfectCandidatesNum.length : 0
-    } member(s) typed "${nCmd}"`
+    } member(s) typed "${target}"`
   );
 }
 
-//generate(100, "hit", 0.03, 5);
-console.log(new Member(50).cmd);
+generate(20, "hit", 0.05, 100);
