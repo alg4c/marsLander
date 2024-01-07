7329ec914aea65f7b24f9bae871e6519304bb856
Alexander <alexgriffith360@gmail.com>
2024-01-07 17:21:49 -0500

    refactor geography file

--- geography.js
+++ geography.js
@@ -1,33 +1,35 @@
-import { drawLine } from "./svg.js";
+class Coordinate {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
-export const geography = {
-  coordinates: [
-    [0, 100],
-    [1000, 500],
-    [1500, 1500],
-    [3000, 1000],
-    [4000, 150],
-    [5500, 150],
-    [6999, 800],
-  ],
-  getLZ() {
-    for (let i = 1; i < this.coordinates.length; i++) {
-      const [x1, y1] = this.coordinates[i - 1];
-      const [x2, y2] = this.coordinates[i];
+class Geography {
+  constructor(...args) {
+    this.coordinates = [];
+    for (let i = 0; i < args.length; i += 2) {
+      this.coordinates.push(new Coordinate(args[i], args[i + 1]));
+    }
+  }
+  get landingZone() {
+    for (let i = 0; i < this.coordinates.length; i += 2) {
+      const { x: x1, y: y1 } = this.coordinates[i];
+      const { x: x2, y: y2 } = this.coordinates[i + 1];
       if (y2 === y1) return { x1, x2, y: y2 };
     }
-  },
-  getSurfaceElevation(x) {
-    const geoCoords = geography.coordinates;
-    for (let i = 1; i < geoCoords.length; i++) {
-      var [x1_1, y1_1] = geoCoords[i - 1];
-      var [x2_1, y2_1] = geoCoords[i];
-      if (x1_1 <= x && x2_1 >= x) break;
+  }
+  calculateElevation(x) {
+    for (let i = 0; i < this.coordinates.length; i += 2) {
+      const { x: x1, y: y1 } = this.coordinates[i];
+      const { x: x2, y: y2 } = this.coordinates[i + 1];
+      if (x >= x1 && x <= x2) {
+        const slope = (y2 - y1) / (x2 - x1);
+        const yIntercept = y1 - slope * x1;
+        return slope * x + yIntercept;
+      }
     }
-    const slope = (y2_1 - y1_1) / (x2_1 - x1_1);
-    const yIntercept = y1_1 - slope * x1_1;
-    return slope * x + yIntercept;
-  },
-};
+  }
+}
 
-drawLine(geography.coordinates, "geoline");
+export default Geography;
